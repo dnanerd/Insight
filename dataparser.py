f94c142df60f89488b30876ef09c1765de73a93e
Diane <diwu.su@gmail.com>
2013-01-27 11:40:42 -0800

    dataparser.py split search into modules and read from file rather than make api call

--- dataparser.py
+++ dataparser.py
@@ -1,38 +1,65 @@
+#!/usr/bin/python
+
 import json
-import yummly
-import pprint
+import yummly #https://github.com/dgilland/yummly.py
 import sys
-
-yummly.api_id = '5dd6a908'
-yummly.api_key = '1144f281d7ac2e4d2f08ba7883bdc396'
-
-results = yummly.search('banana bread')
+#import my_sql
 
 
-outFile = "search.output.txt"
-f = open(outFile,'w')
-printer = pprint.PrettyPrinter(indent=4, stream=f)
-printer.pprint(results)
-#print results["matches"]
-f.close()
+def searchDatabase(query, outFile):
+	results	= yummly.search(query)
 
-outFile = "search.output.parsed.txt"
-f = open(outFile,'w')
-for item in results["matches"]:
-	f.write('New Item:\n')
-	for key, val in item.iteritems():
-		f.write("{0}\t{1}\n".format(key,val))
-	f.write('\n')
-f.close()
+	f = open(outFile,'w')
+	f.write(json.dumps(results, indent=4, separators = (',',': ')))
+	f.close()
+
+
+	print "Keys: ",",".join(results.keys())
+	print "Number of records: ", results["totalMatchCount"]
+	print "Search: ", results["criteria"]
+
+	for key,val in results.iteritems():
+		if (key != "matches"):
+			print "Key: ", key
+			print "Value: ", repr(val)
+		#print "DECODED: ", decoded
+
+	return(results)
+
+
+def pullRecipes(searchResultFile, recipeFile):
+
+	f = open(searchResultFile, 'r')
+	searchResult = json.loads(f.read())
+	f.close()
+
+	recipeList = [];
+	for item in searchResult["matches"]:
+		recipe_id = item['id']
+		recipe = yummly.recipe(recipe_id)
+		recipeList.append(recipe)
+	fr 	= 	open(recipeFile,'w')
+	fr.write(json.dumps(recipeList, indent=4, separators = (',',': ')))
+	fr.close()
+	return recipeList
+
+def parseRecipes(recipeFile):
+	fr = open(recipeFile, 'r')
+	recipeList = json.loads(fr.read())
+	fr.close()
+	for recipe in recipeList:
+		ingredientList = recipe["ingredientLines"]
+
+
 
 #decoded = json.loads(results)
 
-print "Keys: ",",".join(results.keys())
-print "Number of records: ", results["totalMatchCount"]
-print "Search: ", results["criteria"]
 
-for key,val in results.iteritems():
-	if (key != "matches"):
-		print "Key: ", key
-		print "Value: ", repr(val)
-#print "DECODED: ", decoded
+yummly.api_id	=	'5dd6a908'
+yummly.api_key	=	'1144f281d7ac2e4d2f08ba7883bdc396'
+outFile	= "search.output.txt"
+recipeFile = "search.recipes.txt"
+#res = searchDatabase("banana bread", outFile)
+
+#pullRecipes(outFile, recipeFile)
+recipeList = parseRecipes(recipeFile)
